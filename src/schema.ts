ec57931c2af9ee5c4f1265b23ad67dccb7c5e4f4
Hosain <cryptichasan925@gmail.com>
2025-02-06 15:21:31 +0600

    graphql schema updated done

--- src/schema.ts
+++ src/schema.ts
@@ -1,11 +1,28 @@
 export const typeDefs = `#graphql
- 
-  type Book {
-    title: String
-    author: String
-  }
- 
-  type Query {
-    books: [Book]
-  }
+    
+    type Post {
+        id : ID!
+        title : String!
+        content : String!
+        published : Boolean!
+        author : User!
+        createdAt : String!
+    }
+
+    type User {
+        id : ID!
+        name : String!
+        email : String!
+        age : Int
+        createdAt : String!
+        posts : [Post!]
+    }
+
+    type Profile {
+        id : ID!
+        bio : String!
+        user : User!
+        createdAt : String!
+    }
+    
 `;
