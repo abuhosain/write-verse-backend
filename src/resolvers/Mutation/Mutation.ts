21a4a6326918eabdd9fb3bc2f9b8d7ce51ad564b
Hosain <cryptichasan925@gmail.com>
2025-02-09 10:54:36 +0600

    separte resolvers for post

--- src/resolvers/Mutation/Mutation.ts
+++ src/resolvers/Mutation/Mutation.ts
@@ -1,40 +1,7 @@
 import { authResolvers } from "./auth";
-
-export interface IUserInfo {
-  name: string;
-  email: string;
-  password: string;
-  bio?: string;
-}
+import { postResolvers } from "./post";
 
 export const Mutation = {
   ...authResolvers,
-  addPost: async (parent: any, args: any, { prisma, userInfo }: any) => {
-    if (!userInfo) {
-      return {
-        userError: "User not authenticated",
-        post: null,
-      };
-    }
-
-    if (!args.title || !args.content) {
-      return {
-        userError: "Title and content is required",
-        post: null,
-      };
-    }
-
-    const newPost = await prisma.post.create({
-      data: {
-        title: args.title,
-        content: args.content,
-        authorId: userInfo.userId,
-      },
-    });
-
-    return {
-      userError: null,
-      post: newPost,
-    };
-  },
+  ...postResolvers,
 };
