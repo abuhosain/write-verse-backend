c25e51d28cf4bdea3d293328c3df56775862dfa6
Hosain <cryptichasan925@gmail.com>
2025-04-09 23:05:29 +0600

    get me

--- src/resolvers/Query/Query.ts
+++ src/resolvers/Query/Query.ts
@@ -1,8 +1,19 @@
-export const Query = {
-  users: async (parent: any, args: any, { prisma }: any) => {
-    return prisma.user.findMany();
-  },
-posts : async (parent: any, args: any, { prisma }: any) => {
-    return prisma.post.findMany( );
-  }
-};
+  export const Query = {
+      me: async(parent : any,arg : any, {prisma, userInfo} : any) => {
+      return await prisma.user.findUnique({
+        where : {
+          id : userInfo.userId
+        } 
+      });
+      },  
+      users: async (parent: any, args: any, { prisma }: any) => {
+      return prisma.user.findMany();
+    },
+      posts : async (parent: any, args: any, { prisma }: any) => {
+      return prisma.post.findMany({
+        where : {
+          published : true
+        }
+      });
+    }
+  };
